3ee76e365cb4f37bcb4632ce4130567b76e076f9
Arjun Anil <arjunanil319@gmail.com>
2024-07-14 23:40:25 +0530

    Changes added

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
         const olOne=document.getElementById("taskToComplete");
         const olTwo=document.getElementById("taskCompleted");
         const button= document.getElementById("submit");
+        const text=document.getElementById('text');
+
         button.addEventListener('click',e =>{
+                if(text.value.trim()==""){
+                        alert("Enter a task!");
+                }
+                else{
                 const li=document.createElement("li");
-                task=document.getElementById("text").value;
+                task=text.value.trim();
+                text.value="";
                 olOne.appendChild(li);
                 li.innerText=task+'\n';
                 li.classList.add('li');
@@ -21,9 +28,15 @@
                         li.classList.add('li2');
                         li.textContent=task+'\n';
                 })  
+                }
         })
-        
-        
 
- })
+
+        text.addEventListener('keypress',(e)=>{
+                if(e.key=='Enter'){
+                e.preventDefault();
+                button.click();
+                }
+        })
+})
 
